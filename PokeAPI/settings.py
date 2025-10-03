e013e18c0878afe8ec07335e2215d22860d9764b
SUA <samuel.urbano.arana@gmail.com>
2025-10-03 19:09:29 -0400

    nueva variable eb el .env y cambios en el html
    Co-authored-by: Gustavo Heredia  <Gustav0H2O@users.noreply.github.com>

--- PokeAPI/settings.py
+++ PokeAPI/settings.py
@@ -28,6 +28,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = 
 
 ALLOWED_HOSTS = [
+    "logic-breath-turning-rated.trycloudflare.com",
     "127.0.0.1",
     "127.0.0.1:8000",
     "localhost",
@@ -38,6 +39,7 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://logic-breath-turning-rated.trycloudflare.com",
     "http://127.0.0.1",
     "http://127.0.0.1:8000",
     "http://localhost",
